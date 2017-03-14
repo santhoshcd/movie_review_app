dde1faebc339ba5f4c4d754cbd4359a4392e6fb5
Santhosh <santhoshdevadiga.cs@gmail.com>
2017-03-14 19:39:03 +0000

    Update image size

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
 	has_many :reviews
-	has_attached_file :poster, styles: { medium: "200x300#" }
+	has_attached_file :poster, styles: { medium: "300x400#" }
 	validates_attachment_content_type :poster, content_type: /\Aimage\/.*\Z/
 end
