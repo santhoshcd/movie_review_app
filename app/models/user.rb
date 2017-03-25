4c19c2dab4aa12cfc2b27aa058e9da8c61df41ed
Santhosh <santhoshdevadiga.cs@gmail.com>
2017-03-25 09:13:00 +0000

    Add/Update user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,20 +2,22 @@ class User < ApplicationRecord
 	belongs_to :role
 	has_many :reviews
 	has_many :users, through: :reviews
-	before_create :set_default_role
 
 	def self.from_omniauth(auth)
-	    where(auth.slice(provider: auth.provider, uid: auth.uid)).first_or_initialize.tap do |user|
-	      user.provider = auth.provider
-	      user.uid = auth.uid
-	      user.name = auth.info.name
-	      user.email = auth.info.email
-	      user.image_path = auth.info.image
-	      user.oauth_token = auth.credentials.token
-	      user.role ||= Role.find_by_name('customers')
-	      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-	      user.save!
-	    end
+		user = User.find_by_provider_and_uid(auth.provider, auth.uid)
+		if user.blank?
+			user = User.new
+		    user.provider = auth.provider
+		    user.uid = auth.uid
+		    user.name = auth.info.name
+		    user.email = auth.info.email
+		    user.image_path = auth.info.image
+		    user.oauth_token = auth.credentials.token
+		    user.role ||= Role.find_by_name('customers')
+		    user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+		    user.save!
+		end
+		return user    
 	end
 
 	def is_admin?
